c68d3506ec3e12699c9b489086dc41a6e9fafc83
Shoeb Saquib <128196549+shoeb-saquib@users.noreply.github.com>
2025-09-24 19:47:50 -0400

    transferred code to ankle controller class

--- main.py
+++ main.py
@@ -2,55 +2,26 @@ import mujoco as mj
 from mujoco import viewer
 import numpy as np
 import time
+from simple_ankle_controller import SimpleAnkleController
 
 def get_pos(model, qpos, name):
     joint_id = mj.mj_name2id(model, mj.mjtObj.mjOBJ_JOINT, name)
     return qpos[model.jnt_qposadr[joint_id]]
 
-def get_vel(model, data, name):
-    joint_id = mj.mj_name2id(model, mj.mjtObj.mjOBJ_JOINT, name)
-    return data.qvel[model.jnt_dofadr[joint_id]]
-
-def move_block(model, data, name, pos):
+def move_actuator_to_pos(model, data, name, pos):
     curr_pos = get_pos(model, data.qpos, name)
     motor_id = mj.mj_name2id(model, mj.mjtObj.mjOBJ_ACTUATOR, name)
     torque = 100 * (pos - curr_pos)
     data.ctrl[motor_id] = torque
 
-def move_many(model, data, names, qpos):
+def move_all_to_pos(model, data, names, qpos):
     for name in names:
-        move_block(model, data, name, get_pos(model, qpos, name))
-
-def body_pos_xy(model, data, body_name):
-    bid = mj.mj_name2id(model, mj.mjtObj.mjOBJ_BODY, body_name)
-    pos = data.xpos[bid]
-    return np.array([pos[0], pos[1]])
+        move_actuator_to_pos(model, data, name, get_pos(model, qpos, name))
 
 def site_pos_xy(model, data, body_name):
-    bid = mj.mj_name2id(model, mj.mjtObj.mjOBJ_SITE, body_name)
-    pos = data.xpos[bid]
+    sid = mj.mj_name2id(model, mj.mjtObj.mjOBJ_SITE, body_name)
+    pos = data.xpos[sid]
     return np.array([pos[0], pos[1]])
-
-def lin_interp(t, t_total, start, end):
-    return start + (t / t_total) * (end - start)
-
-def get_com(data):
-    return data.subtree_com[0]
-
-def get_gravity_constant(model):
-    return abs(model.opt.gravity[2])
-
-def set_ankle_torque(model, data, torque):
-    left_ankle_act_id = mj.mj_name2id(model, mj.mjtObj.mjOBJ_ACTUATOR, "l_foot_pitch")
-    right_ankle_act_id = mj.mj_name2id(model, mj.mjtObj.mjOBJ_ACTUATOR, "r_foot_pitch")
-    data.ctrl[left_ankle_act_id] = torque
-    data.ctrl[right_ankle_act_id] = torque
-
-def calculate_ankle_torque(model, data, m, g, pz_req, body_name):
-    return 0.5*m*g*(pz_req - body_pos_xy(model, data, body_name)[0])
-
-def apply_ankle_torque(model, data, m, g, pz_req):
-    set_ankle_torque(model, data, calculate_ankle_torque(model, data, m, g, pz_req, "l_foot_pitch"))
 
 def add_random_vels(t, dt, model, data, noise_std, interval):
     cycles = interval // dt
@@ -63,32 +34,17 @@ def simulate():
     data = mj.MjData(model)
     viewer2 = viewer.launch_passive(model, data)
     actuator_names = [mj.mj_id2name(model, mj.mjtObj.mjOBJ_ACTUATOR, i) for i in range(model.nu)]
-    g = get_gravity_constant(model)
-    m = np.sum(model.body_mass)
-    prev_com = get_com(data).copy()
-    com = get_com(data)
     dt = model.opt.timestep
     t = -1
     data.qpos = model.keyframe('home').qpos
-    com_marker = mj.mj_name2id(model, mj.mjtObj.mjOBJ_SITE, "com_marker")
-    desired_com_marker = mj.mj_name2id(model, mj.mjtObj.mjOBJ_SITE, "desired_com_marker")
-    pz_req_marker = mj.mj_name2id(model, mj.mjtObj.mjOBJ_SITE, "pz_req_marker")
-    pz_curr_marker = mj.mj_name2id(model, mj.mjtObj.mjOBJ_SITE, "pz_curr_marker")
+    stabilizer = SimpleAnkleController(model, data)
     while True:
-        add_random_vels(t, dt, model, data, 0.1, 5)
-        move_many(model, data, actuator_names, model.keyframe('home').qpos)
+        #add_random_vels(t, dt, model, data, 0.1, 5)
+        move_all_to_pos(model, data, actuator_names, model.keyframe('home').qpos)
         desired_com = site_pos_xy(model, data, "l_foot")[0] + 0.16
-        com_vel = (com - prev_com) / dt
-        frequency_squared = g / com[2]
-        desired_com_acceleration = (-50 * (com[0] - desired_com) - 5 * com_vel[0]) / m
-        pz_req = com[0] - desired_com_acceleration / frequency_squared
-        apply_ankle_torque(model, data, m, g, pz_req)
-        prev_com = com.copy()
+        stabilizer.step(desired_com)
         mj.mj_step(model, data)
-        data.site_xpos[com_marker] = np.array([com[0], com[1], com[2]])
-        data.site_xpos[desired_com_marker] = np.array([desired_com, com[1], com[2]])
-        data.site_xpos[pz_req_marker] = np.array([pz_req, com[1], 0])
-        data.site_xpos[pz_curr_marker] = np.array([body_pos_xy(model, data, "l_foot_pitch")[0], com[1], 0])
+        stabilizer.show_debug_markers(desired_com)
         t += dt
         time.sleep(dt)
         viewer2.sync()
--- simple_ankle_controller.py
+++ simple_ankle_controller.py
@@ -1,9 +1,51 @@
 import mujoco as mj
 import numpy as np
 
+
 class SimpleAnkleController:
 
     def __init__(self, model, data):
         self.model = model
         self.data = data
 
+        self.dt = model.opt.timestep
+        self.m = np.sum(model.body_mass)
+        self.g = abs(model.opt.gravity[2])
+        self.com = data.subtree_com[0]
+        self.prev_com = self.com.copy()
+
+        self.l_pitch = mj.mj_name2id(model, mj.mjtObj.mjOBJ_ACTUATOR, "l_foot_pitch")
+        self.r_pitch = mj.mj_name2id(model, mj.mjtObj.mjOBJ_ACTUATOR, "r_foot_pitch")
+
+        self.com_marker = mj.mj_name2id(model, mj.mjtObj.mjOBJ_SITE, "com_marker")
+        self.desired_com_marker = mj.mj_name2id(model, mj.mjtObj.mjOBJ_SITE, "desired_com_marker")
+        self.pz_req_marker = mj.mj_name2id(model, mj.mjtObj.mjOBJ_SITE, "pz_req_marker")
+        self.pz_curr_marker = mj.mj_name2id(model, mj.mjtObj.mjOBJ_SITE, "pz_curr_marker")
+
+    def body_pos_xy(self, body_name):
+        bid = mj.mj_name2id(self.model, mj.mjtObj.mjOBJ_BODY, body_name)
+        pos = self.data.xpos[bid]
+        return np.array([pos[0], pos[1]])
+
+    def calculate_ankle_torque(self, pz_req, body_name):
+        return 0.5 * self.m * self.g * (pz_req - self.body_pos_xy(body_name)[0])
+
+    def apply_ankle_torque(self, pz_req):
+        torque = self.calculate_ankle_torque(pz_req, "l_foot_pitch")
+        self.data.ctrl[self.l_pitch] = torque
+        self.data.ctrl[self.r_pitch] = torque
+
+    def show_debug_markers(self, desired_com):
+        com = self.com
+        self.data.site_xpos[self.com_marker] = np.array([com[0], com[1], com[2]])
+        self.data.site_xpos[self.desired_com_marker] = np.array([desired_com, com[1], com[2]])
+        self.data.site_xpos[self.pz_req_marker] = np.array([self.pz_req, com[1], 0])
+        self.data.site_xpos[self.pz_curr_marker] = np.array([self.body_pos_xy("l_foot_pitch")[0], com[1], 0])
+
+    def step(self, desired_com):
+        com_vel = (self.com - self.prev_com) / self.dt
+        omega2 = self.g / self.com[2]
+        desired_com_acceleration = (-50 * (self.com[0] - desired_com) - 5 * com_vel[0]) / self.m
+        self.pz_req = self.com[0] - desired_com_acceleration / omega2
+        self.apply_ankle_torque(self.pz_req)
+        self.prev_com = self.com.copy()
